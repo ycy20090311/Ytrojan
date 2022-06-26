667e4f7acb0b0b6105995d4e325b4c9f6b96a770
孤独的帅哥杨 <87306964+ycy20090311@users.noreply.github.com>
2022-06-26 13:25:04 +0800

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,17 +1,19 @@
-import socket
+import socket,sys
 
 def Caesar(i,string1):#凯撒密码加密与解密
     string2 = ''
     if i == 0:#加密
-        for a in string1:string2 = string2 + chr(ord(a)+319)
+        for a in string1:
+            string2 = string2 + chr(ord(a)+4)
     elif i == 1:#解密
-        for a in string1:string2 = string2 + chr(ord(a)-319)
+        for a in string1:
+            string2 = string2 + chr(ord(a)-4)
     return string2
 
 
 if '__main__' == __name__:
     t = socket.socket()
-    t.bind(('', 12345))
+    t.bind(('',int(sys.argv[1])))
     t.listen(1)
     c,ip = t.accept()
     while True:
@@ -22,3 +24,4 @@ if '__main__' == __name__:
             print(Caesar(1,c.recv(2048).decode()))
     c.close()
     t.close()
+
